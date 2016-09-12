04810dbeaf35799e4209f1c2e0733f6721511862
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2016-09-12 22:17:20 +1200

    added details for confirmation

--- sqlFiles/orderDetails.sql
+++ sqlFiles/orderDetails.sql
@@ -1,4 +1,4 @@
-SELECT size, s.sizeid, s.typeid, t.itemid, type, item FROM sizes s
+SELECT size, s.sizeid, s.typeid, t.itemid, s.size, type, item FROM sizes s
 INNER JOIN types t ON s.typeid=t.typeid
 INNER JOIN items i ON t.itemid=i.itemid
 WHERE s.sizeid=$1
