ec847e52b6b3f39eba7321f6f7525c03df7b9692
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2016-09-20 21:57:30 +1200

    show items in menu

--- controllers/company.js
+++ controllers/company.js
@@ -2,16 +2,24 @@
  * Created by lewis.knoxstreader on 20/09/16.
  */
 
-const express = require('express');
-const router = express.Router();
+const express = require('express'),
+  router = express.Router();
+
+const companyRepo = require('../repositories/CompanyRepository');
 
 router.param('companyId', (req, res, next, id) => {
-  console.log("company id =", id);
-  return next();
+  return companyRepo.getCompanyAndItems(id)
+    .then(data => {
+      if (!data) throw "no company found";
+      console.log("DATA FROM COMPANY QUERY", data);
+      req.company = data;
+      req.fbid = id;
+      return next();
+    });
 });
 
 router.get('/:companyId', (req, res) => {
-  return res.render('company', { title: req.user.name, me: { name: 'bumbler' } });
+  return res.render('company', { title: req.company[0].name, items: req.company, fbid: req.fbid });
 });
 
 module.exports = router;
--- repositories/CompanyRepository.js
+++ repositories/CompanyRepository.js
@@ -2,7 +2,7 @@
  * Created by lewis.knoxstreader on 20/09/16.
  */
 
-const { Company } = require('../database/models/index');
+const { sequelize, Company, Item } = require('../database/models/index');
 
 exports.findUserCompanies = (accounts => {
   return Company.findAll({
@@ -10,4 +10,14 @@ exports.findUserCompanies = (accounts => {
     where: { fbid: { $or: accounts } }
   })
 });
+
+exports.getCompanyAndItems = id => {
+  return sequelize.query(
+    "SELECT name, location, item, items.itemid, type, typeid FROM companies" +
+    " INNER JOIN items ON companies.fbid = items.fbid" +
+    " INNER JOIN types ON items.itemid = types.itemid" +
+    " WHERE companies.fbid = $1",
+    { bind: [id], type: sequelize.QueryTypes.SELECT }
+  );
+};
 
