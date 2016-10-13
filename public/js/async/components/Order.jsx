85d68c189111b97801ec442b381a6ebf238c5203
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2016-10-14 10:05:55 +1300

    each order now has two image boxes (still no working src link) for item photo & for user photo

--- public/js/async/components/Order.jsx
+++ public/js/async/components/Order.jsx
@@ -7,8 +7,9 @@ const Order = ({ onClick, pending, type, size, item, userid, pickuptime }) => (
       textDecoration: pending ? 'none' : 'line-through'
     }}
   >
-    <img src={`https://graph.facebook.com/${userid}/?fields=profile_pic%access_token=EAAP0Oze8Cs0BAKbVdE716FjxC8uJjJSgbTiTSt3vbvsOtSQxFp6Fhv2Bxyi2HaM0t4068MXaS8TiaUkMXnpTlwZAH8xNAHwz3gmfwZAfZAIZAQxiHkSK3vWshzgGECpHXnOWbvD8lReYlx4dUb1h4VnMU1z3mCksRZAV5blqZB1wZDZD`}></img>
-    <img src="typeid"></img>
+    {/*<img src={`https://graph.facebook.com/${userid}/?fields=profile_pic%access_token=EAAP0Oze8Cs0BAKbVdE716FjxC8uJjJSgbTiTSt3vbvsOtSQxFp6Fhv2Bxyi2HaM0t4068MXaS8TiaUkMXnpTlwZAH8xNAHwz3gmfwZAfZAIZAQxiHkSK3vWshzgGECpHXnOWbvD8lReYlx4dUb1h4VnMU1z3mCksRZAV5blqZB1wZDZD`}></img>*/}
+    <img src="user photo"></img>
+    <img src="item photo"></img>
     <span>{size} | {type} | {item} @ {pickuptime}</span>
   </li>
 );
