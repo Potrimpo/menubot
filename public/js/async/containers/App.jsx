aa6c7adb4ad8cf64a1950b6272ba597b005681cf
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2016-10-13 15:53:48 +1300

    filter orders (in local state only)

--- public/js/async/containers/App.jsx
+++ public/js/async/containers/App.jsx
@@ -1,14 +1,16 @@
 import React, { Component, PropTypes } from 'react'
 import Picker from '../components/Picker'
 import OrdersBox from './OrdersBox'
+import Footer from '../components/Footer'
 
 const App = () => (
   <div>
     <OrdersBox/>
-    <Picker
-      value={'placeholder value'}
-      options={[ 'reactjs', 'frontend' ]}
-    />
+    <Footer/>
+    {/*<Picker*/}
+      {/*value={'placeholder value'}*/}
+      {/*options={[ 'reactjs', 'frontend' ]}*/}
+    {/*/>*/}
   </div>
 );
 
