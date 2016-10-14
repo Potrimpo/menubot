8dcdf70b4898577d5adab4a9c0de47ee956ac763
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2016-10-14 13:41:00 +1300

    deleted unused element Picker

--- public/js/async/containers/App.jsx
+++ public/js/async/containers/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component, PropTypes } from 'react'
-import Picker from '../components/Picker'
 import OrdersBox from './OrdersBox'
 import Footer from '../components/Footer'
 
@@ -7,10 +6,6 @@ const App = () => (
   <div>
     <OrdersBox/>
     <Footer/>
-    {/*<Picker*/}
-      {/*value={'placeholder value'}*/}
-      {/*options={[ 'reactjs', 'frontend' ]}*/}
-    {/*/>*/}
   </div>
 );
 
