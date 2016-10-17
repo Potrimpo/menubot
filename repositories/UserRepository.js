e8a44f1c29944466ee4dfb8d7647f7a60bbae2f6
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2016-10-17 17:27:48 +1300

    creating account now fetches access token for every page you own

--- repositories/UserRepository.js
+++ repositories/UserRepository.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var { User } = require('../database/models/index');
+const fetch = require('node-fetch');
 
 exports.getUserById = function(id) {
   return User.findById(id);
@@ -63,23 +64,53 @@ exports.createAccFromFacebook = function(accessToken, refreshToken, profile) {
   var profileId = profile.id.toString();
 
   return User.findOne({ where: { facebookId: profileId } })
-    .then(function(existingUser) {
-      if (existingUser) { return existingUser; }
-      var user = User.build({ facebookId: profileId });
+    .then(existingUser => {
+      if (existingUser) {
+        return existingUser;
+      }
+    })
+    .then(() => {
       const accounts = profile._json.accounts.data.map(company => {
-        return {
-          fbid: company.id,
-          name: company.name
-        };
+        return getPageAccessToken(accessToken, company.id)
+          .then(json => {
+            return {
+              fbid: company.id,
+              name: company.name,
+              token: json.access_token
+            };
+          })
       });
+      return Promise.all(accounts);
+    })
+    .then(accounts => {
+      var user = User.build({facebookId: profileId});
       user.set('accounts', accounts);
       user.email = profile._json.email || ( profileId + '@facebook.com' );
       user.tokens = { facebook: accessToken };
       user.name = profile.name.givenName + ' ' + profile.name.familyName;
       user.photo = 'https://graph.facebook.com/' + profile.id+ '/picture?type=large';
       user.profile = { name: profile._json.first_name };
-
       return user.save();
     })
     .catch(err => console.error("error creating profile", err.message || err));
 };
+
+function getPageAccessToken (userToken, pageId) {
+  pageId = encodeURIComponent(pageId);
+  userToken = encodeURIComponent((userToken));
+  const query = `https://graph.facebook.com/${pageId}?fields=access_token&access_token=${userToken}`;
+  console.log("QUERY =", query);
+  return fetch(query, {
+    method: 'GET',
+    headers: {'Content-Type': 'application/json'}
+  })
+    .then(rsp => rsp.json())
+    .then(json => {
+      if (json.error && json.error.message) {
+        throw new Error(json.error.message);
+      }
+      console.log("json ============= ", json);
+      return json;
+    })
+    .catch(err => console.error("error fetching access token!!", err));
+}
