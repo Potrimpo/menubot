ca3ac1139bea363594b420cc3b6274e8fd0425b0
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2016-10-19 18:03:44 +1300

    fixed bugs with initializing persistent menu & moved functions into their own file

---
+++ controllers/activateAccount.js
@@ -0,0 +1,66 @@
+/**
+ * Created by lewis.knoxstreader on 19/10/16.
+ */
+const fetch = require('node-fetch');
+
+exports.activateBot = pageToken => {
+  return subscribeToWebhook(pageToken)
+    .then(() => initializePersistentMenu(pageToken))
+};
+
+function subscribeToWebhook (pageToken) {
+  pageToken = encodeURIComponent((pageToken));
+  const body = `access_token=${pageToken}`;
+  const query = `https://graph.facebook.com/me/subscribed_apps`;
+  return fetch(query, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body
+  })
+    .then(rsp => rsp.json())
+    .then(json => {
+      if (json.error && json.error.message) {
+        throw new Error(json.error.message);
+      }
+      return json;
+    })
+    .catch(err => console.error("error activating bot for this page!!", err));
+}
+
+function initializePersistentMenu (pageToken) {
+  const body = {
+    setting_type: "call_to_actions",
+    thread_state: "existing_thread",
+    call_to_actions:[
+      {
+        type: "postback",
+        title: "Menu",
+        payload: "MENU"
+      },
+      {
+        type: "postback",
+        title: "My Orders",
+        payload: "MY_ORDERS"
+      },
+      {
+        type: "postback",
+        title: "Location",
+        payload: "LOCATION",
+      }
+    ]
+  };
+  pageToken = encodeURIComponent((pageToken));
+  const url = `https://graph.facebook.com/me/thread_settings?access_token=${pageToken}`;
+  return fetch(url, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(body)
+  })
+    .then(rsp => rsp.json())
+    .then(json => {
+      if (json.error && json.error.message) {
+        throw new Error(json.error.message);
+      }
+      return json;
+    })
+}
--- controllers/api.js
+++ controllers/api.js
@@ -4,7 +4,8 @@ const express = require('express'),
   router = express.Router(),
   fetch = require('node-fetch'),
   { retrieveOrders, setOrderComplete } = require('./orders'),
-  { findCompany, setBotStatus } = require('../repositories/CompanyRepository');
+  { findCompany, setBotStatus } = require('../repositories/CompanyRepository'),
+  { activateBot } = require('./activateAccount');
 
 
 // absolute path is /api/orders/:fbid
@@ -23,67 +24,5 @@ router.route('/activate/:fbid')
       .then(() => setBotStatus(req.params.fbid, true))
       .then(() => res.redirect('/'));
   });
-
-function activateBot(pageToken) {
-  return subscribeToWebhook(pageToken)
-    .then(() => initializePersistentMenu(pageToken))
-}
-
-function subscribeToWebhook (pageToken) {
-  pageToken = encodeURIComponent((pageToken));
-  const body = `access_token=${pageToken}`;
-  const query = `https://graph.facebook.com/me/subscribed_apps`;
-  return fetch(query, {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body
-  })
-    .then(rsp => rsp.json())
-    .then(json => {
-      if (json.error && json.error.message) {
-        throw new Error(json.error.message);
-      }
-      return json;
-    })
-    .catch(err => console.error("error activating bot for this page!!", err));
-}
-
-function initializePersistentMenu (pageToken) {
-  const body = {
-    setting_type: "call_to_actions",
-    thread_state: "existing_thread",
-    call_to_actions:[
-      {
-        type: "postback",
-        title: "Menu",
-        payload: "MENU"
-      },
-      {
-        type: "postback",
-        title: "My Orders",
-        payload: "MY_ORDERS"
-      },
-      {
-        type: "postback",
-        title: "Location",
-        url: "LOCATION",
-      }
-    ]
-  };
-  pageToken = encodeURIComponent((pageToken));
-  const url = `https://graph.facebook.com/me/thread_settings?access_token=${pageToken}`;
-  return fetch(url, {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body
-    })
-    .then(rsp => rsp.json())
-    .then(json => {
-      if (json.error && json.error.message) {
-        throw new Error(json.error.message);
-      }
-      return json;
-    })
-}
 
 module.exports = router;
