872d76c9fe9eacc61c92ed33efba18a9f6613059
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2016-10-19 17:31:49 +1300

    activating bot for page now also adds persistent menu

--- controllers/api.js
+++ controllers/api.js
@@ -19,14 +19,17 @@ router.route('/orders/:fbid')
 router.route('/activate/:fbid')
   .get((req, res) => {
     return findCompany(req.params.fbid)
-      .then(data => {
-        return activateBot(data.access_token);
-      })
+      .then(data => activateBot(data.access_token))
       .then(() => setBotStatus(req.params.fbid, true))
       .then(() => res.redirect('/'));
   });
 
-function activateBot (pageToken) {
+function activateBot(pageToken) {
+  return subscribeToWebhook(pageToken)
+    .then(() => initializePersistentMenu(pageToken))
+}
+
+function subscribeToWebhook (pageToken) {
   pageToken = encodeURIComponent((pageToken));
   const body = `access_token=${pageToken}`;
   const query = `https://graph.facebook.com/me/subscribed_apps`;
@@ -43,6 +46,44 @@ function activateBot (pageToken) {
       return json;
     })
     .catch(err => console.error("error activating bot for this page!!", err));
+}
+
+function initializePersistentMenu (pageToken) {
+  const body = {
+    setting_type: "call_to_actions",
+    thread_state: "existing_thread",
+    call_to_actions:[
+      {
+        type: "postback",
+        title: "Menu",
+        payload: "MENU"
+      },
+      {
+        type: "postback",
+        title: "My Orders",
+        payload: "MY_ORDERS"
+      },
+      {
+        type: "postback",
+        title: "Location",
+        url: "LOCATION",
+      }
+    ]
+  };
+  pageToken = encodeURIComponent((pageToken));
+  const url = `https://graph.facebook.com/me/thread_settings?access_token=${pageToken}`;
+  return fetch(url, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body
+    })
+    .then(rsp => rsp.json())
+    .then(json => {
+      if (json.error && json.error.message) {
+        throw new Error(json.error.message);
+      }
+      return json;
+    })
 }
 
 module.exports = router;
