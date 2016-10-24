c670aad75035dea288a9d32bc1d2c1f95a0fe71d
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2016-10-24 13:02:38 +1300

    changing witSessions to be vastly more efficient

--- witSessions.js
+++ witSessions.js
@@ -8,31 +8,22 @@ const { getCompanyAccessToken } = require('./repositories/site/CompanyRepository
 const sessions = {};
 
 const findOrCreateSession = (fbUserId, fbPageId) => {
-  let sessionId;
-  // Let's see if we already have a session for the user fbUserId
-  Object.keys(sessions).forEach(k => {
-    if (sessions[k].fbUserId === fbUserId) {
-      // return a promise even though this isnt really async
-      // because getCompanyAccessToken returns a promise
-      // so we need to be able to call .then() on the return value of findOrCreateSession
-      return new Promise((res, rej) => res(k));
-    }
-  });
-  if (!sessionId) {
-    sessionId = new Date().toISOString();
-    // getting page access token so the server knows which page is supposed to be responding
-    return getCompanyAccessToken(fbPageId)
-      .then(data => {
-        sessions[sessionId] = {
-          fbUserId,
-          fbPageId,
-          access_token: data.access_token,
-          context: {}
-        };
-        return sessionId;
-      })
-      .catch(e => console.error("error generating session for bot interaction!!", e));
+  console.log('sessions.fbUserId =', sessions[fbUserId]);
+  if (sessions[fbUserId]) {
+    return new Promise((res, rej) => res(fbUserId));
   }
+  console.log("---->     creating new session      <----");
+  return getCompanyAccessToken(fbPageId)
+    .then(data => {
+      sessions[fbUserId] = {
+        fbUserId,
+        fbPageId,
+        access_token: data.access_token,
+        context: {}
+      };
+      return fbUserId;
+    })
+    .catch(e => console.error("error generating session for bot interaction!!", e));
 };
 
 module.exports = {
