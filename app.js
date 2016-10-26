d7a39b21fba581a76b46a1ffaf1e48d19ec87611
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2016-10-26 20:58:46 +1300

    set postgres connection ssl = true

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.use(cookieParser());
 //PostgreSQL Store
 app.use(session({
   store: new pgSession({
-    conString: `postgres://postgres:${postgresPassword}@${postgresURL}:5432/menubot`,
+    conString: `postgres://postgres:${postgresPassword}@${postgresURL}:5432/menubot?ssl=true`,
     tableName: sessionTable
   }),
   secret: secrets.sessionSecret,
