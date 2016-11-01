764a348b549a9a1c60750dd877b0f39845698184
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2016-11-02 10:18:37 +1300

    rename to customer_name, & no closing <img> needed

--- public/js/async/components/Order.jsx
+++ public/js/async/components/Order.jsx
@@ -1,11 +1,11 @@
 import React, { PropTypes } from 'react'
 
-const Order = ({ onClick, pending, type, size, item, userid, pickuptime, photo, profile_pic, profile_name }) => (
+const Order = ({ onClick, pending, type, size, item, userid, pickuptime, photo, profile_pic, customer_name }) => (
   <div onClick={onClick} style={{ textDecoration: pending ? 'none' : 'line-through' }} className="row">
     <div className="col-lg-offset-3 col-lg-6 col-md-offset-3 col-md-6 col-sm-offset-2 col-sm-8 col-xs-12 order-active-container">
       <div className="row">
         <div className="order-content-container">
-          <img className="order-photo" src={photo}></img>
+          <img className="order-photo" src={photo}/>
         </div>
         <div className="order-content-container">
           <p className="order-text">- {size}</p>
@@ -16,10 +16,10 @@ const Order = ({ onClick, pending, type, size, item, userid, pickuptime, photo, 
       <div className="order-line"></div>
       <div className="row">
         <div className="order-content-container">
-          <img className="order-photo" src={profile_pic}></img>
+          <img className="order-photo" src={profile_pic}/>
         </div>
         <div className="order-content-container">
-          <p className="order-text">Orderer: <strong>{profile_name}</strong></p>
+          <p className="order-text">Orderer: <strong>{customer_name}</strong></p>
           <p className="order-text">Pickup @: <strong>{pickuptime}</strong></p>
         </div>
       </div>
