6614b85ae57796672d5d98487e52e5634c946e6c
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2016-11-01 16:33:49 +1300

    show item photo in list of orders

--- public/js/async/components/Order.jsx
+++ public/js/async/components/Order.jsx
@@ -1,15 +1,14 @@
 import React, { PropTypes } from 'react'
 
-const Order = ({ onClick, pending, type, size, item, userid, pickuptime }) => (
+const Order = ({ onClick, pending, photo, type, size, item, userid, pickuptime }) => (
   <li
     onClick={onClick}
     style={{
       textDecoration: pending ? 'none' : 'line-through'
     }}
   >
-    {/*<img src={`https://graph.facebook.com/${userid}/?fields=profile_pic%access_token=EAAP0Oze8Cs0BAKbVdE716FjxC8uJjJSgbTiTSt3vbvsOtSQxFp6Fhv2Bxyi2HaM0t4068MXaS8TiaUkMXnpTlwZAH8xNAHwz3gmfwZAfZAIZAQxiHkSK3vWshzgGECpHXnOWbvD8lReYlx4dUb1h4VnMU1z3mCksRZAV5blqZB1wZDZD`}></img>*/}
     <img src="user photo"></img>
-    <img src="item photo"></img>
+    <img src={photo}></img>
     <span>{size} | {type} | {item} @ {pickuptime}</span>
   </li>
 );
