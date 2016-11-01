8a705d2f69cac46c5e36f5d5d132b35fdd0216d2
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2016-11-02 11:23:59 +1300

    changing environment variables file

--- envVariables.js
+++ envVariables.js
@@ -2,19 +2,16 @@ module.exports = {
   PORT: process.env.PORT || 8445,
 
   // remember to change webhook url @ developers.facebook.com/apps/{app-id}/webhooks/
-  tunnelURL: 'https://menubot.ngrok.io', // use https && no slash at end (~.com vs ~.com/)
+  tunnelURL: process.env.tunnelURL || '', // use https && no slash at end (~.com vs ~.com/)
 
   // link to postgres database
   postgresURL: process.env.postgresURL || 'localhost',
-  postgresPassword: '45FeistyEggsFearY2k',
+  postgresPassword: process.env.postgresPassword,
 
   serverIP: process.env.serverIP || 'localhost',
   sessionTable: 'session',
 
-  WIT_TOKEN: 'U2XQR7JKYCAN44AECAEOTA5I2DIASK23',
-
   // FACEBOOK: APP = MENUBOT
-  FB_PAGE_TOKEN: 'EAAP0Oze8Cs0BAM1H4QC8aGcTXbOHFVpRhNHjM3ODO6TW3tRTi3Khyy2VhSg43Eorq85qgJW9CVFl9QXRruQHABPacXTC1LrZCAi7mPzhjP4KR6mF6dH5ZAxwEGkOZCfr9BTZAZCk5wi1nE68KcTZBvf9l3SjlEmZApPqRgJ130W4QZDZD',
   FB_APP_SECRET: '096eac9cb0c0ad06dd3204440515febc',
   FB_VERIFY_TOKEN: 'saveme',
 
