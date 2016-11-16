44ca4da0782c419c8f17ee6d1605ab0e22002810
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2016-11-16 16:54:00 +1300

    don’t show empty lines when order has no type/size

--- public/jsx/orders/components/Order.jsx
+++ public/jsx/orders/components/Order.jsx
@@ -1,16 +1,16 @@
 import React, { PropTypes } from 'react'
 
 const Order = ({ onClick, pending, type, size, item, userid, pickuptime, photo, profile_pic, customer_name }) => (
-  <div onClick={onClick} style={{ textDecoration: pending ? 'none' : 'line-through' }} className="row">
+  <div onClick={onClick} className="row">
     <div className="col-lg-offset-3 col-lg-6 col-md-offset-3 col-md-6 col-sm-offset-2 col-sm-8 col-xs-12 order-active-container">
       <div className="row">
         <div className="order-content-container">
           <img className="order-photo" src={photo}/>
         </div>
         <div className="order-content-container">
-          <p className="order-text">- {size}</p>
-          <p className="order-text">- {type}</p>
-          <p className="order-text">- {item}</p>
+          { size ? <p className="order-text">- {size}</p> : null }
+          { type ? <p className="order-text">- {type}</p> : null }
+          { item ? <p className="order-text">- {item}</p> : null }
         </div>
       </div>
       <div className="order-line"></div>
