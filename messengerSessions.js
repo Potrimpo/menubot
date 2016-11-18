61f7801fb314caa5b9106ffb6c9d29eb296b4b8a
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2016-11-19 12:53:47 +1300

    sends data to redis when creating new session

--- messengerSessions.js
+++ messengerSessions.js
@@ -1,13 +1,28 @@
-const { getCompanyAccessToken } = require('./repositories/site/CompanyRepository'),
+const redis = require('redis'),
+  bluebird = require('bluebird'),
+  { getCompanyAccessToken } = require('./repositories/site/CompanyRepository'),
   { findOrCreateCustomer } = require('./repositories/bot/botQueries');
 
-// This will contain all user sessions.
-// Each session has an entry:
-// sessionId -> {fbid, context, access_token}
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+
+const client = redis.createClient();
+
+client.onAsync('connect').then(() => console.log('----> redis connected'));
+
 const sessions = {};
 
 const findOrCreateSession = (fbUserId, fbPageId) => {
   console.log('sessions.fbUserId =', sessions[fbUserId]);
+  client.hgetallAsync(fbUserId)
+    .then(data => {
+      console.log("after checking redis");
+      if (data) {
+        console.log("found in redis:", data);
+        return null;
+      }
+    });
+
   if (sessions[fbUserId]) {
     return new Promise((res, rej) => res(fbUserId));
   }
@@ -23,6 +38,10 @@ const findOrCreateSession = (fbUserId, fbPageId) => {
         access_token: data.access_token,
         context: {}
       };
+      return client.hmsetAsync(fbUserId, 'pageID', fbPageId, 'access_token', data.access_token)
+    })
+    .then(data => {
+      console.log("redis set status", data);
       return fbUserId;
     })
     .catch(e => console.error("error generating session for bot interaction!!", e));
