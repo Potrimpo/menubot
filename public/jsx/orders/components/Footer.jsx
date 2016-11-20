7703e5f98c24e5a5fd494893f8be6eb455f5b635
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2016-11-20 13:38:46 +1300

    fixed CSS not showing on orders list filter buttons

--- public/jsx/orders/components/Footer.jsx
+++ public/jsx/orders/components/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => (
     <a href="/" className="btn btn-default btn-space" role="button">
       <i className="fa fa-long-arrow-left" style={{fontSize: "18px"}} aria-hidden="true"></i>
     </a>
-    <div></div>
-    <p className="btn btn-default active btn-space" style={{cursor: "default"}} role="button">Show: </p>
-    <FilterLink filter="SHOW_ACTIVE"><p className="btn btn-default btn-space" role="button">Active</p></FilterLink>
-    <FilterLink filter="SHOW_COMPLETED"><p className="btn btn-default btn-space" role="button">Completed</p></FilterLink>
+    <div>
+      <FilterLink filter="SHOW_ACTIVE"><p className="btn btn-default btn-space" role="button">Active</p></FilterLink>
+      <FilterLink filter="SHOW_COMPLETED"><p className="btn btn-default btn-space" role="button">Completed</p></FilterLink>
+    </div>
   </div>
 );
 
