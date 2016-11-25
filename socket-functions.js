8d0b39fbf07a263feeaf14290e189f734ab8380e
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2016-11-25 22:26:54 +1300

    discard dead redis pub/sub subscriptions on socket disconnect

--- socket-functions.js
+++ socket-functions.js
@@ -1,7 +1,7 @@
 /**
  * Created by lewis.knoxstreader on 20/11/16.
  */
-const { sub } = require('./redis-init'),
+const { client, sub } = require('./redis-init'),
   { fetchOrders, setOrderComplete } = require('./controllers/orders');
 
 function init (io) {
@@ -14,15 +14,36 @@ function init (io) {
 
     socket.on('order-status', orderid => setOrderComplete(orderid));
 
+    socket.on("disconnect", disco);
   });
+
 }
 
+// --> Register interest in new orders for our page
+// create redis entry for our fbid, indexed by the socket id
+// subscribe to redis events on our fbid's frequency
 function requestOrders (fbid) {
-  sub.subscribe(fbid);
+  console.log("subscribing");
+  client.setAsync(this.id, fbid)
+    .then(() => sub.subscribe(fbid))
+    .catch(err => {
+      console.error("error setting redis socket session", err);
+      return sub.unsubscribe(fbid);
+    });
 
   return fetchOrders(fbid)
     .then(orders => this.emit('orders-list', orders))
     .catch(err => console.error("error in socket business", err));
+}
+
+// --> Disconnect socket & stop listening for new-order updates for our page
+// find the right fbid by searching redis with our socket id
+// unsubscribe from events on fbid frequency
+function disco () {
+  client.getAsync(this.id)
+    .then(fbid => sub.unsubscribe(fbid))
+    .then(() => client.delAsync(socket.id))
+    .catch(err => console.error("error disconnecting socket", err));
 }
 
 module.exports = init;
