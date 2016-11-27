007ecaf21d7e46b330c7f1f14cc819fc3005cf1a
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2016-11-28 12:21:06 +1300

    no menu photos in orders list & smaller customer photo (saving space)

--- public/jsx/orders/components/Order.jsx
+++ public/jsx/orders/components/Order.jsx
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react'
 
-const Order = ({onClick, pending, type, size, item, item_photo,type_photo, userid, pickuptime, profile_pic, customer_name }) => (
+const Order = ({onClick, pending, type, size, item, userid, pickuptime, profile_pic, customer_name }) => (
   <div onClick={onClick} className="row">
     <div
       className="col-lg-offset-3 col-lg-6 col-md-offset-3 col-md-6 col-sm-offset-2 col-sm-8 col-xs-12 order-basic-container"
@@ -8,16 +8,6 @@ const Order = ({onClick, pending, type, size, item, item_photo,type_photo, useri
     >
       <div className="row">
         <div className="order-content-container">
-          { item_photo ?
-            <div className="order-content-container">
-              <img className="order-photo" src={item_photo}/>
-            </div> : null
-          }
-          { type_photo ?
-            <div className="order-content-container">
-              <img className="order-photo" src={type_photo}/>
-            </div> : null
-          }
           { item ? <p className="order-text">- {item}</p> : null }
           { type ? <p className="order-text">- {type}</p> : null }
           { size ? <p className="order-text">- {size}</p> : null }
@@ -29,8 +19,8 @@ const Order = ({onClick, pending, type, size, item, item_photo,type_photo, useri
           <img className="order-photo" src={profile_pic}/>
         </div>
         <div className="order-content-container">
-          <p className="order-text">Orderer: <strong>{customer_name}</strong></p>
-          <p className="order-text">Pickup @: <strong>{pickuptime}</strong></p>
+          <p className="order-text"><strong>{customer_name}</strong></p>
+          <p className="order-text">@: <strong>{pickuptime}</strong></p>
         </div>
       </div>
     </div>
