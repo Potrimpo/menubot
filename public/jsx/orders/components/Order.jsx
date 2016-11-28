a333ff30347808abd10459a405261eb0b59e589a
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2016-11-29 12:11:24 +1300

    flipped order appearance - Photo & name at the top

--- public/jsx/orders/components/Order.jsx
+++ public/jsx/orders/components/Order.jsx
@@ -4,18 +4,11 @@ const Order = ({onClick, pending, child, type, size, item, userid, pickuptime, p
   <div onClick={onClick} className="row">
     <div
       className="col-lg-offset-3 col-lg-6 col-md-offset-3 col-md-6 col-sm-offset-2 col-sm-8 col-xs-12 order-basic-container"
-      style={ pending ? {backgroundColor:"#e0e0e0", border: "1px solid #b5b5b5" } : {backgroundColor:"#f2f2f2", border: "1px solid #e8e8e8"} }
+      style={
+        pending ? {backgroundColor:"#e0e0e0", border: "1px solid #b5b5b5" } :
+        {backgroundColor:"#f2f2f2", border: "1px solid #e8e8e8"}
+      }
     >
-      <div className="row">
-        <div className="order-content-container">
-          { item ? <p className="order-text">- {item}</p> : null }
-          { type ? <p className="order-text">- {type}</p> : null }
-          { size ? <p className="order-text">- {size}</p> : null }
-        </div>
-      </div>
-
-      <div className="order-line"></div>
-
       { child ? null :
         <div className="row">
           <div className="order-content-container">
@@ -27,6 +20,16 @@ const Order = ({onClick, pending, child, type, size, item, userid, pickuptime, p
           </div>
         </div>
       }
+
+      <div className="order-line"></div>
+
+      <div className="row">
+        <div className="order-content-container">
+          { item ? <p className="order-text">- {item}</p> : null }
+          { type ? <p className="order-text">- {type}</p> : null }
+          { size ? <p className="order-text">- {size}</p> : null }
+        </div>
+      </div>
     </div>
   </div>
 );
