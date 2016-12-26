1cce265a98e308c0b51053be576468344a79af9e
DavidDaish <daviddaish@zoinks.gen.nz>
2016-12-26 14:16:43 +1300

    Logs with additions

--- messaging/text-messages.js
+++ messaging/text-messages.js
@@ -56,7 +56,7 @@ function isTooLate (closetime) {
   console.log("new Date() = " + new Date());
   console.log(new Date());
   console.log("------");
-
+  console.log(chrono.parseDate(closetime + "GMT+1300"));
   console.log("chrono.parseDate(closetime) = " + chrono.parseDate(closetime));
   console.log(chrono.parseDate(closetime));
   console.log("------");
