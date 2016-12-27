bb77d047ea3d281eae5cd0dae62f90af40ccd5bd
DavidDaish <daviddaish@zoinks.gen.nz>
2016-12-27 16:23:41 +1300

    Test failed

--- messaging/response-logic/runActions.js
+++ messaging/response-logic/runActions.js
@@ -10,18 +10,7 @@ function runActions (fbUserId, fbPageId, msg) {
   // the only text messages it knows how to deals with are order times
   const time = chrono.parseDate(msg);
   if (time) {
-    return db.checkTimezone(fbPageId)
-      .then(data => {
-        if (process.env.NODE_ENV == 'production') {
-          var correctedTime = chrono.parseDate(msg + data.timezone);
-          console.log("Time before parse = "+msg + data.timezone);
-          console.log("Corrected time = "+correctedTime);
-          console.log("Uncorrected time = " + time);
-          return actions.orderTime(fbUserId, fbPageId, correctedTime)
-        }
-        console.log("Just normal time = "+time);
-        return actions.orderTime(fbUserId, fbPageId, time)
-      })
+    return actions.orderTime(fbUserId, fbPageId, time)
       .then(order => order.toMessage())
       .catch(err => {
         console.error("error in runActions", err);
