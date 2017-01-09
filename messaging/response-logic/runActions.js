45c6ab5c58396acfdfd8d609de40668b57e4753a
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2017-01-09 15:44:49 +1300

    remove code duplication for time formatting/parsing

--- messaging/response-logic/runActions.js
+++ messaging/response-logic/runActions.js
@@ -1,17 +1,9 @@
-/**
- * Created by lewis.knoxstreader on 24/10/16.
- */
-
-const chrono = require('chrono-node'),
-  moment = require('moment-timezone'),
+const { formatAndParseTime } = require('../time-management'),
   actions = require('./actions');
 
 function runActions (fbUserId, fbPageId, msg, timestamp, timezone) {
   // the only text messages it knows how to deals with are order times
-  const messageDate = moment.tz(timestamp, timezone);
-  const dateFormat = messageDate.format("M/D/YYYY");
-  const dateZone = messageDate.format("ZZ");
-  const requestedPickup = chrono.parseDate(dateFormat + " " + msg + " " + dateZone);
+  const requestedPickup = formatAndParseTime(msg, timestamp, timezone);
   if (requestedPickup) {
     return actions.orderTime(fbUserId, fbPageId, requestedPickup, timestamp, timezone)
       .then(order => order.toMessage())
