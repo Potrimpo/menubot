747307d1274f60561ae3ca3ffec5514f7151585f
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2017-01-11 17:04:17 +1300

    turned postbackHandler into something less monolithic

--- messaging/response-logic/postbackHandler.js
+++ messaging/response-logic/postbackHandler.js
@@ -2,60 +2,70 @@ const structured = require('./../structured-messages'),
   text = require('./../text-messages'),
   db = require('./../../repositories/bot/botQueries');
 
-function postbackHandler (jsonPayload, fbUserId, fbPageId, timestamp) {
-  return new Promise(function (res, rej) {
-    const payload = JSON.parse(jsonPayload);
+// JSON -> {} -> Number -> Promise
+const postbackHandler = (jsonPayload, ids, timestamp) => {
+  const payload = JSON.parse(jsonPayload);
+  return intentSwitch(payload, ids, timestamp);
+};
 
-    switch (payload.intent.toUpperCase()) {
-      case 'MENU':
-        return db.getMenu(fbPageId)
-          .then((menu) => res(structured.items(menu)) )
-          .catch(err => rej(err));
+// {} -> {} -> Number -> Promise
+const intentSwitch = (payload, ids, timestamp) => {
+  switch (payload.intent.toUpperCase()) {
+    case 'MENU':
+      return getMenu(ids.pageId);
 
-      case 'LOCATION':
-        return db.findLocation(fbPageId)
-          .then(data => res(text.location(data.location)))
-          .catch(err => rej(err));
+    case 'LOCATION':
+      return getLocation(ids.pageId);
 
-      case 'DETAILS':
-        return db.getTypes(payload.itemid)
-          .then(types => res(structured.types(types, payload.itemid)) )
-          .catch(err => rej(err));
+    case 'DETAILS':
+      return getTypes(payload.itemid);
 
-      case 'SIZES':
-        return db.getSizes(payload.typeid)
-          .then(sizes => res(structured.sizes(sizes, payload.typeid, payload.itemid)) )
-          .catch(err => rej(err));
+    case 'SIZES':
+      return getSizes(payload.typeid, payload.itemid);
 
-      case 'ORDER':
-        return db.checkOpenStatus(fbPageId)
-          .then(status => text.openStatus(status, fbUserId, payload, timestamp))
-          .then(resp => res(resp))
-          .catch(err => rej(err));
+    case 'ORDER':
+      return placeOrder(ids.pageId, ids.userId, payload, timestamp);
 
-      case 'MY_ORDERS':
-        return db.ordersbyUserid(fbUserId)
-          .then(xs =>
-            res(text.hasOrders(xs)))
-          .catch(err => rej(err));
+    case 'MY_ORDERS':
+      return myOrders(ids.userId);
 
-      case 'HOURS':
-        return db.checkOpenStatus(fbPageId)
-          .then(data => res(text.hours(data)))
-          .catch(err => rej(err));
+    case 'HOURS':
+      return getHours(ids.pageId);
 
-      case 'GET_STARTED':
-       return res(structured.getStarted());
+    case 'GET_STARTED':
+      return new Promise(res => res(structured.getStarted()));
 
-      default:
-        return rej(new Error("couldn't deal with this postbackHandler input"));
-    }
+    default:
+      return new Promise((_, rej) => rej(new Error("couldn't deal with this postbackHandler input")));
+  }
+};
 
-  })
-    .catch(err => {
-      const payload = JSON.parse(jsonPayload);
-      console.error(`Error in ${payload.intent} postback:`, err)
-    });
-}
+const getMenu = pageId =>
+  db.getMenu(pageId)
+    .then(menu => structured.items(menu));
+
+const getLocation = pageId =>
+  db.findLocation(pageId)
+    .then(data => text.location(data.location));
+
+const getTypes = itemid =>
+  db.getTypes(itemid)
+    .then(types => structured.types(types, itemid));
+
+const getSizes = (typeid, itemid) =>
+  db.getSizes(typeid)
+    .then(sizes => structured.sizes(sizes, typeid, itemid));
+
+const placeOrder = (pageId, userId, payload, timestamp) =>
+  db.checkOpenStatus(pageId)
+    .then(status => text.openStatus(status, userId, payload, timestamp));
+
+const myOrders = userId =>
+  db.ordersbyUserid(userId)
+    .then(xs => text.hasOrders(xs));
+
+const getHours = pageId =>
+  db.checkOpenStatus(pageId)
+    .then(data => text.hours(data));
 
 module.exports = postbackHandler;
