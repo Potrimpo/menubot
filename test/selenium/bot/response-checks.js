8ef37292c9fd93d86c9d1ee62d76c7c34bcc98a6
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2017-01-12 17:32:45 +1300

    more tests for bot: checks all persistent menu buttons

--- test/selenium/bot/response-checks.js
+++ test/selenium/bot/response-checks.js
@@ -2,11 +2,12 @@ const expect = require('chai').expect,
   dev = require('../../../config/local-dev-variables'),
   txt = require('../../../messaging/message-list');
 
-describe('messenger interaction', function() {
+describe('bot response-checks', function() {
   this.timeout(20000);
 
+  // go to messenger & login
   before(function () {
-    browser.url(`https://www.messenger.com/t/${dev.testPageID}`);
+    browser.url('https://www.messenger.com/');
     const title = browser.getTitle();
     expect(title).to.equal('Messenger');
 
@@ -17,31 +18,18 @@ describe('messenger interaction', function() {
     passwordField.setValue(dev.password);
 
     passwordField.submitForm();
+  });
+
+  // get past the get-started button
+  beforeEach(function () {
+    browser.url(`https://www.messenger.com/t/${dev.testPageID}`);
 
     const getStarted = 'div._2xh4._59w_';
-    browser.waitForExist(getStarted);
-    browser.click(getStarted);
+    waitAndDo(getStarted, 'click');
   });
 
-  it('check welcome message', function () {
-    const welcomeDiv = `span=${txt.welcome}`;
-    browser.waitForExist(welcomeDiv);
-  });
-
-  it('get the menu', function () {
-    const persistentMenuButton = 'a._3km2';
-    browser.waitForExist(persistentMenuButton);
-    browser.click(persistentMenuButton);
-
-    const menuButton = 'a=Menu';
-    browser.waitForExist(menuButton);
-    browser.click(menuButton);
-
-    const structuredMessage = 'div._3cn0';
-    browser.waitForExist(structuredMessage);
-  });
-
-  after(function () {
+  // delete conversation to start afresh
+  afterEach(function () {
     const conversationActions = '#js_4 div._5blh._4-0h._p';
     browser.click(conversationActions);
 
@@ -51,6 +39,60 @@ describe('messenger interaction', function() {
     const bigRedDelete = 'button._3quh._30yy._2t_._3ay_._5ixy';
     browser.waitForExist(bigRedDelete);
     browser.click(bigRedDelete);
-  })
+  });
+
+  it('check welcome message', function () {
+    const welcomeDiv = msg(txt.welcome);
+    waitAndDo(welcomeDiv);
+  });
+
+  describe('persistent-menu response-checks', function () {
+
+    it('check menu: expects items', function () {
+      persistentMenuSelect('Menu');
+
+      const structuredMessage = 'div._3cn0';
+      waitAndDo(structuredMessage);
+    });
+
+    it('check myOrders: expects none', function () {
+      persistentMenuSelect('My Orders');
+
+      const returned = msg(txt.noOrders);
+      waitAndDo(returned);
+    });
+
+    it('check location: expects not known', function () {
+      persistentMenuSelect('Location');
+
+      const noLocation = msg(txt.locationCheck.notFound);
+      waitAndDo(noLocation);
+    });
+
+    it('check hours: expects closed', function () {
+      persistentMenuSelect('Hours');
+
+      const hours = msg(txt.hoursCheck.closed);
+      waitAndDo(hours);
+    });
+
+  });
 
 });
+
+const msg = txt => `span=${txt}`;
+
+// have to pass action as string
+// passing browser.click first class glitches the scope
+const waitAndDo = (selector, action) => {
+  browser.waitForExist(selector);
+  if (action) browser[action](selector);
+};
+
+const persistentMenuSelect = type => {
+  const persistentMenuButton = 'a._3km2';
+  waitAndDo(persistentMenuButton, 'click');
+
+  const button = `a=${type}`;
+  waitAndDo(button, 'click');
+};
