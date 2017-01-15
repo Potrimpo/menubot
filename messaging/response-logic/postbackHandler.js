c84129024bcfae78205ad8b42093f1ffab12682b
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2017-01-15 17:47:44 +1300

    fixed up message interpreter w cleaner code

--- messaging/response-logic/postbackHandler.js
+++ messaging/response-logic/postbackHandler.js
@@ -40,32 +40,4 @@ const intentSwitch = (payload, ids, timestamp) => {
   }
 };
 
-const getMenu = pageId =>
-  db.getMenu(pageId)
-    .then(menu => structured.items(menu));
-
-const getLocation = pageId =>
-  db.findLocation(pageId)
-    .then(data => text.location(data.location));
-
-const getTypes = itemid =>
-  db.getTypes(itemid)
-    .then(types => structured.types(types, itemid));
-
-const getSizes = (typeid, itemid) =>
-  db.getSizes(typeid)
-    .then(sizes => structured.sizes(sizes, typeid, itemid));
-
-const placeOrder = (pageId, userId, payload, timestamp) =>
-  db.checkOpenStatus(pageId)
-    .then(status => text.openStatus(status, userId, payload, timestamp));
-
-const myOrders = userId =>
-  db.ordersbyUserid(userId)
-    .then(xs => text.hasOrders(xs));
-
-const getHours = pageId =>
-  db.checkOpenStatus(pageId)
-    .then(data => text.hours(data));
-
 module.exports = postbackHandler;
--- messaging/response-logic/runActions.js
+++ messaging/response-logic/runActions.js
@@ -1,44 +1,32 @@
 const actions = require('./actions'),
-  chrono = require('chrono-node'),
   time = require('../time-management'),
-  text = require('./../text-messages'),
+  commands = require('../bot-commands'),
   QR = require('../quick-replies'),
-  txt = require('../message-list'),
-  structured = require('./../structured-messages'),
-  db = require('./../../repositories/bot/botQueries');
+  txt = require('../message-list');
 
 function runActions (fbUserId, fbPageId, msg, timestamp, timezone) {
   const reducedMsg = msg.toLowerCase().replace(/\s+/g, '');
   const requestedPickup = time.orderDateTime(msg, timestamp, timezone);
 
-  if (reducedMsg == "menu") {
-    return db.getMenu(fbPageId)
-      .then(menu => structured.items(menu));
+  switch (reducedMsg) {
+    case "menu":
+      return commands.getMenu(fbPageId);
+    case "location":
+      return commands.getLocation(fbPageId);
+    case "myorders" || "orders" || "myorder":
+      return commands.myOrders(fbUserId);
+    case "hours" || "hour":
+      return commands.getHours(fbPageId);
+    case requestedPickup:
+      return actions.orderTime(fbUserId, fbPageId, requestedPickup, timestamp, timezone)
+        .then(order => order.toMessage())
+        .catch(err => {
+          console.error("error in runActions", err);
+          throw {text: txt.orderAttempt.error, quick_replies: QR.basicReplies};
+        });
+    default:
+      return actions.defaultResp();
   }
-  else if (reducedMsg == "location") {
-    return db.findLocation(fbPageId)
-      .then(data => text.location(data.location));
-  }
-  else if (reducedMsg == "myorders" ||
-           reducedMsg == "orders" ||
-           reducedMsg == "myorder") {
-    return db.ordersbyUserid(fbUserId)
-      .then(xs => text.hasOrders(xs));
-  }
-  else if (reducedMsg == "hours" ||
-           reducedMsg == "hour") {
-    return db.checkOpenStatus(fbPageId)
-      .then(data => text.hours(data));
-  }
-  else if (chrono.parseDate(msg)) {
-    return actions.orderTime(fbUserId, fbPageId, requestedPickup, timestamp, timezone)
-      .then(order => order.toMessage())
-      .catch(err => {
-        console.error("error in runActions", err);
-        throw {text: txt.orderAttempt.error, quick_replies: QR.basicReplies};
-      });
-  }
-  return actions.defaultResp();
 }
 
 module.exports = runActions;
