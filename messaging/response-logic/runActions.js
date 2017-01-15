c46d94c638e43fbb0b36d51126114e410e59fd63
David Daish <daviddaish@zoinks.gen.nz>
2017-01-15 17:17:02 +1300

    Front end changes

--- messaging/response-logic/runActions.js
+++ messaging/response-logic/runActions.js
@@ -1,16 +1,41 @@
 const actions = require('./actions'),
+  chrono = require('chrono-node'),
   time = require('../time-management'),
-  txt = require('../message-list');
+  text = require('./../text-messages'),
+  QR = require('../quick-replies'),
+  txt = require('../message-list'),
+  structured = require('./../structured-messages'),
+  db = require('./../../repositories/bot/botQueries');
 
 function runActions (fbUserId, fbPageId, msg, timestamp, timezone) {
-  // the only text messages it knows how to deals with are order times
+  const reducedMsg = msg.toLowerCase().replace(/\s+/g, '');
   const requestedPickup = time.orderDateTime(msg, timestamp, timezone);
-  if (requestedPickup) {
+
+  if (reducedMsg == "menu") {
+    return db.getMenu(fbPageId)
+      .then(menu => structured.items(menu));
+  }
+  else if (reducedMsg == "location") {
+    return db.findLocation(fbPageId)
+      .then(data => text.location(data.location));
+  }
+  else if (reducedMsg == "myorders" ||
+           reducedMsg == "orders" ||
+           reducedMsg == "myorder") {
+    return db.ordersbyUserid(fbUserId)
+      .then(xs => text.hasOrders(xs));
+  }
+  else if (reducedMsg == "hours" ||
+           reducedMsg == "hour") {
+    return db.checkOpenStatus(fbPageId)
+      .then(data => text.hours(data));
+  }
+  else if (chrono.parseDate(msg)) {
     return actions.orderTime(fbUserId, fbPageId, requestedPickup, timestamp, timezone)
       .then(order => order.toMessage())
       .catch(err => {
         console.error("error in runActions", err);
-        throw txt.orderAttempt.error;
+        throw {text: txt.orderAttempt.error, quick_replies: QR.basicReplies};
       });
   }
   return actions.defaultResp();
